82eda065168889c4a973a38912c9aa0abef7148b
owent <admin@owent.net>
2020-03-22 21:00:15 +0800

    Test disable wd4800 on MSVC 14 and lower

--- 3rd_party/protobuf/protobuf.cmake
+++ 3rd_party/protobuf/protobuf.cmake
@@ -79,7 +79,7 @@ if (NOT 3RD_PARTY_PROTOBUF_BIN_PROTOC OR (NOT 3RD_PARTY_PROTOBUF_LINK_NAME AND N
             if ( CMAKE_CXX_COMPILER_VERSION VERSION_LESS "5.0")
                 set (3RD_PARTY_PROTOBUF_VERSION "3.5.1")
             endif()
-        elseif( ${CMAKE_CXX_COMPILER_ID} STREQUAL "MSVC")
+        elseif(MSVC)
             if ( MSVC_VERSION LESS 1900)
                 set (3RD_PARTY_PROTOBUF_VERSION "3.5.1")
             endif()
