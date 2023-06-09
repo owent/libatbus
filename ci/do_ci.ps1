4cbec45e56341c3a99190e161ac8dc5570b1b849
owent <admin@owent.net>
2023-06-09 09:56:11 +0800

    Fix unit test

--- ci/do_ci.ps1
+++ ci/do_ci.ps1
@@ -55,6 +55,7 @@ if ( $RUN_MODE -eq "msvc.2019+.test" ) {
   Set-Location "build_jobs_ci"
   & cmake ".." "-G" "$Env:CMAKE_GENERATOR" "-A" $Env:CMAKE_PLATFORM "-DBUILD_SHARED_LIBS=$ENV:BUILD_SHARED_LIBS"                        `
     "-DPROJECT_ENABLE_UNITTEST=ON" "-DPROJECT_ENABLE_SAMPLE=ON" "-DPROJECT_ENABLE_TOOLS=ON" "-DATBUS_MACRO_ABORT_ON_PROTECTED_ERROR=ON" `
+    "-DATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PROTOBUF_ALLOW_SHARED_LIBS=OFF"                                                            `
     "-DCMAKE_SYSTEM_VERSION=$selectWinSDKVersion" "-DATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LOW_MEMORY_MODE=ON"
   if ( $LastExitCode -ne 0 ) {
     exit $LastExitCode
@@ -82,6 +83,7 @@ elseif ( $RUN_MODE -eq "msvc.2017.test" ) {
   Set-Location "build_jobs_ci"
   & cmake ".." "-G" "$Env:CMAKE_GENERATOR" "-DBUILD_SHARED_LIBS=$Env:BUILD_SHARED_LIBS" "-DPROJECT_ENABLE_UNITTEST=ON"  `
     "-DPROJECT_ENABLE_SAMPLE=ON" "-DPROJECT_ENABLE_TOOLS=ON" "-DATBUS_MACRO_ABORT_ON_PROTECTED_ERROR=ON"                `
+    "-DATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_PROTOBUF_ALLOW_SHARED_LIBS=OFF"                                            `
     "-DCMAKE_SYSTEM_VERSION=$selectWinSDKVersion" "-DATFRAMEWORK_CMAKE_TOOLSET_THIRD_PARTY_LOW_MEMORY_MODE=ON"
   if ( $LastExitCode -ne 0 ) {
     exit $LastExitCode
