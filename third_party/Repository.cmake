6a6ea2b15e7f4072931b7a065c6841981c812043
owent <admin@owent.net>
2025-12-27 02:05:36 +0800

    Fix deps

--- third_party/Repository.cmake
+++ third_party/Repository.cmake
@@ -1,4 +1,5 @@
 project_third_party_include_port("compression/import.cmake")
+project_third_party_include_port("ssl/port.cmake")
 project_third_party_include_port("libuv/libuv.cmake")
 project_third_party_include_port("fmtlib/fmtlib.cmake")
 project_third_party_include_port("abseil-cpp/abseil-cpp.cmake")
